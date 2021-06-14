33b94ba66ffc218950e9c24b720e6024fda40b73
Sara <sara.m.adamski@gmail.com>
2021-06-14 13:25:15 -0500

    create the homepage view using handlebars.js

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
+const path = require('path');
 const express = require('express');
 // use router instance from routes folder's index.js files
-const routes = require('./routes');
+const routes = require('./controllers/');
 // import the connection to Sequelize from config folder
 const sequelize = require('./config/connection.js');
+// Set up Handlebars.js as the app's template engine of choice
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // turn on routes
 app.use(routes);
